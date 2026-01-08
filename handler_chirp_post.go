4857953962066e71d5af9dae27a77cdd146e87d4
swokamoto <swokamoto@gmail.com>
2026-01-08 13:12:40 -0500

    ts chirp

--- handler_chirp_post.go
+++ handler_chirp_post.go
@@ -1,4 +1,4 @@
-package make
+package main
 
 import (
 	"encoding/json"
@@ -6,19 +6,21 @@ import (
 	"time"
 	"strings"
 	"github.com/google/uuid"
+	"github.com/swokamoto/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerChirpCreate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string    `json:"body"`
-		UserID  uuid.UUID `json:"user_id"`
+		UserID  uuid.NullUUID `json:"user_id"`
 	}
 	type response struct {
 		ChirpID uuid.UUID `json:"chirp_id"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
-		UserID	uuid.UUID `json:"user_id"`
 		Body    string    `json:"body"`
+		UserID	uuid.UUID `json:"user_id"`
+		
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -44,7 +46,7 @@ func (cfg *apiConfig) handlerChirpCreate(w http.ResponseWriter, r *http.Request)
 	cleaned := getCleanedBody(params.Body, badWords)
 
 	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
-		Body:   params.Body,
+		Body:   cleaned,
 		UserID: params.UserID,
 	})
 	if err != nil {
@@ -52,12 +54,18 @@ func (cfg *apiConfig) handlerChirpCreate(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	userID := uuid.UUID{}
+    if chirp.UserID.Valid {
+        userID = chirp.UserID.UUID
+    }
+
 	respondWithJSON(w, http.StatusCreated, response{
 		ChirpID:   chirp.ID,
 		CreatedAt: chirp.CreatedAt,
 		UpdatedAt: chirp.UpdatedAt,
-		UserID:    chirp.UserID,
 		Body:      cleaned,
+		UserID:    userID,
+		
 	})	
 }
 
